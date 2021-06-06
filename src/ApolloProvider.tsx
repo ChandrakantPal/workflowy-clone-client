a39f9d0a44be4ee63a461d41bbe35295712cad54
ChandrakantPal <sanjay161097@gmail.com>
2021-06-06 19:12:15 +0530

    apollo provider setup

---
+++ src/ApolloProvider.tsx
@@ -0,0 +1,31 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  ApolloProvider,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:5000',
+})
+
+const authLink = setContext(() => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
+const Provider: React.FC = ({ children }) => {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>
+}
+
+export default Provider
