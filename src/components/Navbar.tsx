cbaa7456a2b49dad3c38c6e693e8c35e00c888f8
ChandrakantPal <sanjay161097@gmail.com>
2021-06-07 01:16:50 +0530

    nav links added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,12 @@
+import { Link, useLocation } from 'react-router-dom'
 import logo from '../assets/logo.svg'
+import { useAuthDispatch, useAuthState } from '../context/Auth'
 
 const Navbar = () => {
+  const { authenticated } = useAuthState()
+  const dispatch = useAuthDispatch()
+  const location = useLocation()
+
   return (
     <header className="fixed top-0 left-0 right-0 w-full h-14">
       <nav className="flex items-center justify-between p-6">
@@ -8,6 +14,22 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="object-contain w-6" />{' '}
           <p className="ml-3 text-lg font-bold">WorkFlowy</p>
         </div>
+        {authenticated ? (
+          <button
+            onClick={() => dispatch('LOGOUT')}
+            className="px-2 py-1 bg-gray-200 rounded-full "
+          >
+            Logout
+          </button>
+        ) : location.pathname === '/login' ? (
+          <Link to="/signup" className="px-2 py-1 bg-gray-200 rounded-full ">
+            Signup
+          </Link>
+        ) : (
+          <Link to="/login" className="px-2 py-1 bg-gray-200 rounded-full ">
+            Login
+          </Link>
+        )}
       </nav>
     </header>
   )
