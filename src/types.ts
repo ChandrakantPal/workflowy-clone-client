b0eb4066fb44c01d789ed7e2be06c75506a010bb
ChandrakantPal <sanjay161097@gmail.com>
2021-06-07 01:16:15 +0530

    task type

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,18 @@ export interface User {
   username: string
   createdAt: string
 }
+
+export interface Task {
+  body?: string
+  createdAt?: string
+  id?: string
+  isDone?: boolean
+  isRoot?: boolean
+  subTasks?: SubTask[]
+  username?: string
+}
+
+export interface SubTask {
+  subTaskId?: string
+  subTaskTitle?: string
+}
