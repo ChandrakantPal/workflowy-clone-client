ce5ca0ac8a36fca57be72bf6a7842bcf7448f338
ChandrakantPal <sanjay161097@gmail.com>
2021-06-08 00:03:34 +0530

    edit task component used and task component made responsive

--- src/components/TaskComponent.tsx
+++ src/components/TaskComponent.tsx
@@ -1,16 +1,12 @@
 import { FC, useEffect, useState } from 'react'
 import { Task } from '../types'
-import {
-  ChevronDownIcon,
-  ChevronRightIcon,
-  PencilIcon,
-  CheckIcon,
-} from '@heroicons/react/outline'
+import { ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/outline'
 import { Link } from 'react-router-dom'
 import { gql, useLazyQuery } from '@apollo/client'
 import { DeleteTask } from './DeleteTask'
 import classNames from 'classnames'
 import { TaskDone } from './TaskDone'
+import { EditTask } from './EditTask'
 
 interface TaskProps {
   task: Task
@@ -41,34 +37,37 @@ export const TaskComponent: FC<TaskProps> = ({ task, refetch }) => {
   console.log({ expand, subTask, task })
 
   return (
-    <div className="flex flex-col w-full ml-10">
+    <div className="flex flex-col flex-wrap w-full ml-2 md:ml-10">
       <div className="flex items-center group">
-        <div className="flex items-center justify-end w-28">
+        <div className="flex items-center justify-start w-auto md:justify-end md:w-28">
           <DeleteTask taskId={task.id} refetch={refetch} />
-          <PencilIcon className="object-contain w-5 mx-1 opacity-0 cursor-pointer group-hover:opacity-100" />
+          <EditTask taskId={task.id} refetch={refetch} body={task.body} />
           <TaskDone taskId={task.id} refetch={refetch} isDone={task.isDone} />
           {task.subTasks.length > 0 &&
             (expand ? (
               <ChevronDownIcon
-                className="object-contain w-5 mx-1 cursor-pointer"
+                className="object-contain w-3 mx-0.5 md:w-5 md:mx-1 cursor-pointer"
                 onClick={() => setExpand((prevExpand) => !prevExpand)}
               />
             ) : (
               <ChevronRightIcon
-                className="object-contain w-5 mx-1 cursor-pointer"
+                className="object-contain w-3 mx-0.5 md:w-5 md:mx-1 cursor-pointer"
                 onClick={() => setExpand((prevExpand) => !prevExpand)}
               />
             ))}
         </div>
-        <div className="flex items-center justify-center w-4 h-4 bg-gray-300 rounded-full hover:bg-gray-400">
-          <Link to={`/task/${task.id}`} className="text-2xl text-black">
+        <div className="flex items-center justify-center w-3 h-3 bg-gray-300 rounded-full md:w-4 md:h-4 hover:bg-gray-400">
+          <Link to={`/task/${task.id}`} className="text-black md:text-2xl">
             •
           </Link>
         </div>
         <p
-          className={classNames('mx-1 justify-items-end', {
-            'line-through': task.isDone,
-          })}
+          className={classNames(
+            'mx-1 md:justify-items-end justify-items-start md:text-base text-xs',
+            {
+              'line-through': task.isDone,
+            }
+          )}
         >
           {task.body}
         </p>
