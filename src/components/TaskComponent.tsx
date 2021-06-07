94846e23b33114bbb6feeb2633562d01cae30b9f
ChandrakantPal <sanjay161097@gmail.com>
2021-06-07 21:01:30 +0530

    delete task component added and get sub task query

--- src/components/TaskComponent.tsx
+++ src/components/TaskComponent.tsx
@@ -1,25 +1,94 @@
-import { FC } from 'react'
-import { SubTask, Task } from '../types'
-import { DotsHorizontalIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import { FC, useEffect, useState } from 'react'
+import { Task } from '../types'
+import {
+  ChevronDownIcon,
+  ChevronRightIcon,
+  PencilIcon,
+} from '@heroicons/react/outline'
 import { Link } from 'react-router-dom'
+import { gql, useLazyQuery } from '@apollo/client'
+import { DeleteTask } from './DeleteTask'
 
 interface TaskProps {
   task: Task
+  refetch?: () => void
 }
 
-export const TaskComponent: FC<TaskProps> = ({ task }) => {
+export const TaskComponent: FC<TaskProps> = ({ task, refetch }) => {
+  const [expand, setExpand] = useState(false)
+  const [subTask, setSubTask] = useState<Task[]>([])
+  const [GetSubTasks] = useLazyQuery(GET_SUB_TASKS, {
+    onCompleted: (data) => {
+      console.log(data)
+      setSubTask(data.getSubTasks)
+    },
+    fetchPolicy: 'cache-and-network',
+  })
+
+  const fetchSubTasks = () => {
+    GetSubTasks({ variables: { subTaskIds: task.subTasks } })
+  }
+
+  useEffect(() => {
+    if (expand) {
+      GetSubTasks({ variables: { subTaskIds: task.subTasks } })
+    }
+  }, [GetSubTasks, expand, task.subTasks])
+
+  console.log({ expand, subTask, task })
+
   return (
-    <div className="flex items-center justify-evenly">
-      <DotsHorizontalIcon className="object-contain w-5 mx-1" />
-      {task.subTasks.length > 0 && (
-        <ChevronRightIcon className="object-contain w-5 mx-1" />
-      )}
-      <div className="flex items-center justify-center w-4 h-4 bg-gray-300 rounded-full hover:bg-gray-400">
-        <Link to={`/task/${task.id}`} className="text-2xl text-black">
-          •
-        </Link>
+    <div className="flex flex-col ml-10">
+      <div className="relative flex items-center justify-evenly group">
+        <div className="flex items-center justify-start w-20 text-left">
+          <DeleteTask taskId={task.id} refetch={refetch} />
+          <PencilIcon className="object-contain w-5 mx-1 opacity-0 group-hover:opacity-100" />
+          {task.subTasks.length > 0 &&
+            (expand ? (
+              <ChevronDownIcon
+                className="object-contain w-5 mx-1"
+                onClick={() => setExpand((prevExpand) => !prevExpand)}
+              />
+            ) : (
+              <ChevronRightIcon
+                className="object-contain w-5 mx-1"
+                onClick={() => setExpand((prevExpand) => !prevExpand)}
+              />
+            ))}
+        </div>
+        <div className="flex items-center justify-center w-4 h-4 bg-gray-300 rounded-full hover:bg-gray-400">
+          <Link to={`/task/${task.id}`} className="text-2xl text-black">
+            •
+          </Link>
+        </div>
+        <p className="mx-1 justify-items-end">{task.body}</p>
       </div>
-      <p className="mx-1">{task.body}</p>
+      {expand &&
+        subTask.length > 0 &&
+        subTask.map((item) => (
+          <TaskComponent key={item.id} task={item} refetch={fetchSubTasks} />
+        ))}
     </div>
   )
 }
+
+const GET_SUB_TASKS = gql`
+  query getSubTasks($subTaskIds: [IdList!]!) {
+    getSubTasks(subTaskIds: $subTaskIds) {
+      id
+      body
+      createdAt
+      username
+      subTasks {
+        subTaskId
+        subTaskTitle
+      }
+    }
+  }
+`
+
+const DELETE_TASK = gql`
+  mutation deleteTask($taskId: ID!) {
+    deleteTask(taskId: $taskId)
+  }
+`
